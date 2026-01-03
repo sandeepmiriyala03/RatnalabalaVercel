7ea2cae4e8a200a3db804af6898ecf388b49a4db
Sandeep Miriyala <41898282+sandeepmiriyala03@users.noreply.github.com>
2026-01-03 18:51:11 +0530

    commmit code

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -12,8 +12,7 @@ const navItems = [
   { label: "మిరా క్విజ్", path: "/MIRIAQuiz" },
   { label: "రత్నభావాలు", path: "/AboutAuthor" },
   { label: "పద్యాలవాల", path: "/poems" },
-  { label: "పదాలు", path: "/PoemTitles" },
-  { label: "ధ్వని దృశ్యం", path: "/images" },
+
   { label: "అంకితం", path: "/Dedication" },
 ];
 
