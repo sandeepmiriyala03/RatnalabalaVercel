18a6576b21ee5a82e964f138c6b197887e89f8df
Sandeep Miriyala <41898282+sandeepmiriyala03@users.noreply.github.com>
2026-01-10 11:49:44 +0530

    Poetry

--- types/poetry.ts
+++ types/poetry.ts
@@ -15,14 +15,14 @@ export const POETRY_COLLECTIONS: PoetryMeta[] = [
 
   {
     key: "Jandhyala",
-    label: "తెలుగుబాల శతకం",
+    label: "తెలుగుబాల",
     authors: "జంధ్యాల పాపయ్య శాస్త్రి",
   },
 
   
   {
     key: "Sumati",
-    label: "సుమతీ శతకము",
+    label: "సుమతీ",
     authors: "బద్దెన",
   },
 ];
