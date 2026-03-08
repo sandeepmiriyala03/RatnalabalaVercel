0927b93534169163ec66a9a4942c161659799e68
Sandeep Miriyala <41898282+sandeepmiriyala03@users.noreply.github.com>
2026-03-08 10:50:05 +0530

    /khatiMala

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -28,7 +28,7 @@ const navItems = [
   { label: "చిత్రమాల", path: "/chitramala" },
   { label: "స్వరమాల", path: "/swaramala" },
   { label: "లిపిమాల", path: "/lipimala" },
-  { label: "ఖతిమాల", path: "/khatimala" },
+  { label: "ఖతిమాల", path: "/khatiMala" },
 
   // ✅ Google Feedback Form
   {
