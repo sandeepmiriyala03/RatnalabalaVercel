896b38daa971931a785bead19bdd6cbe38750376
Sandeep Miriyala <41898282+sandeepmiriyala03@users.noreply.github.com>
2026-05-04 17:55:29 +0530

    Agent is enbled

--- app/components/YuktaiClient.tsx
+++ app/components/YuktaiClient.tsx
@@ -13,7 +13,7 @@ export default function YuktaiClient({ children }: { children: ReactNode }) {
   if (!mounted) return <>{children}</>;
 
   return (
-    <YuktAIWrapper position="left" showRag={true}>
+    <YuktAIWrapper position="left" showRag={true} showAgent={true}>
       {children}
     </YuktAIWrapper>
   );
